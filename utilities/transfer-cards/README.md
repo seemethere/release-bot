16c763a124a1d63c4b76dd542a2ad0b2f5f2a71b
Eli Uriegas <eli.uriegas@docker.com>
2017-08-17 14:43:04 -0700

    Finish complete project transfering
    Signed-off-by: Eli Uriegas <eli.uriegas@docker.com>

---
+++ utilities/transfer-cards/README.md
@@ -0,0 +1,18 @@
+## Building
+
+```shell
+make clean build
+```
+
+## Example Usage
+
+```shell
+GITHUB_TOKEN=<TOKEN> build/transfer-cards 17.07.0-ce-rc3 17.07.1-ce-rc1
+```
+
+## Help
+
+```shell
+GITHUB_TOKEN=<TOKEN> build/transfer-cards --help
+```
+
--- utilities/transfer-cards/main.go
+++ utilities/transfer-cards/main.go
@@ -17,14 +17,15 @@ var (
 	sourceProjectName      = kingpin.Arg("source-project", "Name of the project to pull cards from").Required().String()
 	destProjectName        = kingpin.Arg("destination-project", "Name of the project to put cards into").Required().String()
 	dryrun                 = kingpin.Flag("dry-run", "Don't make any changes upstream").Bool()
-	columnsToMove          = kingpin.Flag("columns", "Columns to pull from, comma separated").Short('c').Default("Triage,Cherry Picked").String()
+	columnsToMove          = kingpin.Flag("columns", "Columns to pull from, comma separated").Short('c').Default("Triage,Cherry Pick").String()
 	repoName               = kingpin.Flag("repo-name", "Name of the repository to point to").Short('r').Default("staging-release-tracking").String()
 	repoOwner              = kingpin.Flag("repo-owner", "Name of the owner of the repository to point to").Short('o').Default("docker").String()
+	verbose                = kingpin.Flag("verbose", "See debug statements").Short('v').Bool()
 )
 
 func getProject(client *github.Client, ctx context.Context, projectName string) (*github.Project, error) {
-	log.Debug("Attempting to find project %s for repo %s/%s", projectName, *repoOwner, *repoName)
-	projects, _, err := client.Repositories.ListProjects(ctx, *repoOwner, *repoName, nil)
+	log.Debugf("Attempting to find project %s for repo %s/%s", projectName, *repoOwner, *repoName)
+	projects, _, err := client.Repositories.ListProjects(ctx, *repoOwner, *repoName, &github.ProjectListOptions{State: "all"})
 	if err != nil {
 		log.Errorf("Could not grab existing projects for %s/%s: %v", *repoOwner, *repoName, err)
 		os.Exit(1)
@@ -46,6 +47,15 @@ func getColumnID(columnToFind string, columnHaystack []*github.ProjectColumn) (i
 	return 0, fmt.Errorf("column %s not found!", columnToFind)
 }
 
+func getRelatedIssue(card *github.ProjectCard, issues []*github.Issue) (*github.Issue, error) {
+	for _, issue := range issues {
+		if *card.ContentURL == *issue.URL {
+			return issue, nil
+		}
+	}
+	return nil, fmt.Errorf("card %s not related to an existing issue", *card.URL)
+}
+
 func moveIssues(client *github.Client, ctx context.Context, sourceProject, destProject *github.Project, columns []string) {
 	sourceColumns, _, err := client.Projects.ListProjectColumns(ctx, *sourceProject.ID, nil)
 	if err != nil {
@@ -55,6 +65,11 @@ func moveIssues(client *github.Client, ctx context.Context, sourceProject, destP
 	destColumns, _, err := client.Projects.ListProjectColumns(ctx, *destProject.ID, nil)
 	if err != nil {
 		log.Errorf("Error grabbing columns for project %s: %v", *destProject.Name, err)
+		os.Exit(1)
+	}
+	issues, _, err := client.Issues.ListByRepo(ctx, *repoOwner, *repoName, nil)
+	if err != nil {
+		log.Errorf("Error grabbing issues for repo: %v", err)
 		os.Exit(1)
 	}
 	for _, column := range columns {
@@ -73,12 +88,37 @@ func moveIssues(client *github.Client, ctx context.Context, sourceProject, destP
 			log.Errorf("Error retrieving source project cards")
 			os.Exit(1)
 		}
+		for _, card := range sourceCards {
+			relatedIssue, err := getRelatedIssue(card, issues)
+			if err != nil {
+				log.Errorf("%v", err)
+				os.Exit(1)
+			}
+			prefix := "(dryrun) "
+			if !*dryrun {
+				prefix = ""
+				_, err = client.Projects.DeleteProjectCard(ctx, *card.ID)
+				if err != nil {
+					log.Errorf("Error deleting project card %s: %v", *card.URL, err)
+					os.Exit(1)
+				}
+				_, _, err = client.Projects.CreateProjectCard(ctx, destColumnID, &github.ProjectCardOptions{ContentID: *relatedIssue.ID, ContentType: "Issue"})
+				if err != nil {
+					log.Errorf("Error creating project card for issue #%d: %v", *relatedIssue.Number, err)
+					os.Exit(1)
+				}
+			}
+			log.Infof("%s%s/%s -> %s/%s: #%d", prefix, *sourceProject.Name, column, *destProject.Name, column, *relatedIssue.Number)
+		}
 	}
 }
 
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: os.Getenv(githubTokenEnvVariable)},
@@ -94,8 +134,6 @@ func main() {
 		log.Errorf("Destination %v", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Source project: %v, Dest Project: %v\n", *sourceProject.Name, *destProject.Name)
-	for ndx, arg := range strings.Split(*columnsToMove, ",") {
-		fmt.Printf("%v: %s\n", ndx, arg)
-	}
+	log.Infof("Source project: %v, Dest Project: %v", *sourceProject.Name, *destProject.Name)
+	moveIssues(client, ctx, sourceProject, destProject, strings.Split(*columnsToMove, ","))
 }
