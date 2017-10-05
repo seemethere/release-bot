bbea1d719baf2d8d066ec164b40af5182227ec0d
Eli Uriegas <eli.uriegas@docker.com>
2017-10-05 12:11:11 -0700

    Update vendor to include project update options
    Signed-off-by: Eli Uriegas <eli.uriegas@docker.com>

--- utilities/transfer-cards/vendor/golang.org/x/oauth2/internal/doc.go
+++ utilities/transfer-cards/vendor/golang.org/x/oauth2/internal/doc.go
@@ -2,13 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package socket
-
-func (h *msghdr) setIov(vs []iovec) {
-	l := len(vs)
-	if l == 0 {
-		return
-	}
-	h.Iov = &vs[0]
-	h.Iovlen = uint32(l)
-}
+// Package internal contains support packages for oauth2 package.
+package internal
